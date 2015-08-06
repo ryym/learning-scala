dbd07efad0305a8d3381820ed35ab5505214b227
ryym <ryym.64@gmail.com>
2015-08-06 21:29:29 +0900

    Operator as a method

---
+++ src/operators.scala
@@ -0,0 +1,9 @@
+// In scala, all operators are defined as method.
+
+println(1 + 1, 1.+(1))
+println(2 < 3, 2.<(3))
+println(-3, 3.unary_-)
+
+println(true || false, true.||(false))
+println(!true, true.unary_!, ! true)
+
