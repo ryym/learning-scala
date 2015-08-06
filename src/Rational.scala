07ef4816aab2e43d415f7f1321ac67eeaa3d2e87
ryym <ryym.64@gmail.com>
2015-08-06 22:07:44 +0900

    Normalize Rational number

--- src/Rational.scala
+++ src/Rational.scala
@@ -3,8 +3,9 @@ class Rational(n: Int, d: Int) {
   require(d != 0) // Defined in Predef object
 
   // Fields must be defined explicitly.
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
   // Auxiliary constructor must call another constructor.
   def this(n: Int) = this(n, 1)
@@ -21,4 +22,7 @@ class Rational(n: Int, d: Int) {
   def max(that: Rational) = if (this.lessThan(that)) that else this
 
   override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
