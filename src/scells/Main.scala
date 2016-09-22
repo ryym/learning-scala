497268a1c8fc5506c150f4a73aea79c1be5c9e3d
ryym <ryym.64@gmail.com>
2016-09-22 21:08:58 +0900

    scells: Create simple table

---
+++ src/scells/Main.scala
@@ -0,0 +1,10 @@
+package scells
+
+import swing._
+
+object Main extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "ScalaSheet"
+    contents = new Spreadsheet(100, 26)
+  }
+}
